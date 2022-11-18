54c354dac266b2950b5a79e8394b04156791fe43
Trey Hardcastle <treyhardcastle@gmail.com>
2022-11-18 15:15:15 -0800

    its finished

---
+++ assets/script.js
@@ -0,0 +1,27 @@
+var requestUrl = "http://www.omdbapi.com/?&apikey=e2499fec&t=";
+var button = document.querySelector(".btn");
+var cardText = document.querySelector(".card-body");
+var searchText = document.querySelector(".form-control");
+var title = document.querySelector("#title");
+var year = document.querySelector("#year");
+var rating = document.querySelector("#rating");
+var genre = document.querySelector("#genre");
+var plot = document.querySelector("#plot");
+
+function getApi() {
+  var input = searchText.value;
+  var search = requestUrl.concat(input + "&");
+  fetch(search)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      title.textContent = "Title: " + data.Title;
+      year.textContent = "Title: " + data.Year;
+      rating.textContent = "Rating: " + data.Rated;
+      genre.textContent = "Genre: " + data.Genre;
+      plot.textContent = "Summary: " + data.Plot;
+      console.log(data);
+    });
+}
+button.addEventListener("click", getApi);
